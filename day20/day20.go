fb8636d0aa458e4d6d38ef8ac4f701b6615d287b
Neil Vass <neil.vass+github@gmail.com>
2025-11-05 10:08:19 +0000

    Day 20 done

--- day20/day20.go
+++ day20/day20.go
@@ -14,18 +14,20 @@ var puzzleData string
 func main() {
 	var target int
 	input.Parse(regexp.MustCompile(`^(\d+)\n$`), puzzleData, &target)
-	fmt.Printf("Part 1: %d\n", Find(target))
+	fmt.Printf("Part 1: %d\n", Find(target, Deliveries))
+	fmt.Printf("Part 2: %d\n", Find(target, DeliveriesPart2))
 }
 
-func Find(target int) int {
+func Find(target int, deliveryFn func(int) int) int {
 	i := 100_000
 	for {
-		if Deliveries(i) >= target {
+		if deliveryFn(i) >= target {
 			return i
 		}
 		i++
 	}
 }
+
 func Deliveries(house int) int {
 	var count int
 	for _, factor := range FactorsOf(house) {
@@ -48,4 +50,14 @@ func FactorsOf(house int) []int {
 		factors = append(factors, house)
 	}
 	return factors
+}
+
+func DeliveriesPart2(house int) int {
+	var count int
+	for _, factor := range FactorsOf(house) {
+		if house <= factor*50 {
+			count += 11 * factor
+		}
+	}
+	return count
 }
--- day20/day20_test.go
+++ day20/day20_test.go
@@ -38,6 +38,7 @@ func TestFactorsOf(t *testing.T) {
 		{2, []int{1, 2}},
 		{4, []int{1, 2, 4}},
 		{8, []int{1, 2, 4, 8}},
+		{100, []int{1, 2, 50, 4, 25, 5, 20, 10, 100}},
 	}
 	for _, tt := range tests {
 		t.Run(strconv.Itoa(tt.n), func(t *testing.T) {
@@ -45,6 +46,26 @@ func TestFactorsOf(t *testing.T) {
 			diff := cmp.Diff(tt.want, got)
 			if diff != "" {
 				t.Errorf("Contents mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+// In part 2, elves deliver (11 * their number) presents to a max of 50 houses.
+func TestDeliveriesPart2(t *testing.T) {
+	tests := []struct {
+		house int
+		want  int
+	}{
+		{1, 11},
+		{2, 33},
+		{100, (2*11 + 50*11 + 4*11 + 25*11 + 5*11 + 20*11 + 10*11 + 100*11)}, // Elf 1 has been to 50 houses already
+	}
+	for _, tt := range tests {
+		t.Run(strconv.Itoa(tt.house), func(t *testing.T) {
+			got := DeliveriesPart2(tt.house)
+			if got != tt.want {
+				t.Errorf("Deliveries() = %v, want %v", got, tt.want)
 			}
 		})
 	}
